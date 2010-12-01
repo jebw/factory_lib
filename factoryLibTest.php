fdf0ab5cba8439259975dfa308227c51a98b5dfc
jebw <jeb@jdwilkins.co.uk>
2010-12-01 20:36:45 +0000

    initial commit

---
+++ factoryLibTest.php
@@ -0,0 +1,100 @@
+<?php
+
+/*
+ * Edit the connection details to the database in the tests setUp function
+ *
+ * The factory_lib_test database needs a table with the following structure
+ *
+ CREATE TABLE `person` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `forename` varchar(100) DEFAULT NULL,
+  `surname` varchar(100) NOT NULL,
+  `email_address` varchar(100) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+  ) DEFAULT CHARSET=utf8;
+ */
+
+require_once 'PHPUnit/Framework.php' ;
+require dirname(__FILE__) . "/factory_lib.php" ;
+
+Factory::$factory_data['person'] = array('forename' => 'Jeremy {{counter}}', 'surname' => 'Wilkins', 
+																					'email_address' => 'test{{counter}}@testhost.com') ;
+
+class factoryLibTest extends PHPUnit_Framework_TestCase {
+	function setUp() {
+		mysql_connect() ;
+		mysql_select_db('factory_lib_test') ;
+		mysql_query("TRUNCATE person") || die("UNABLE TO TRUNCATE PERSON TABLE") ;
+		Factory::reset_counter() ;
+	}
+	
+	function tearDown() {
+		mysql_close() ;
+	}
+	
+	function testInitialisesValues() {
+		$p = new Factory('person') ;
+		$this->assertEquals('Jeremy 1', $p->get('forename')) ;
+		$this->assertEquals('Wilkins', $p->get('surname')) ;
+		$this->assertNull($p->get('age')) ;
+		$this->assertEquals('test1@testhost.com', $p->get('email_address')) ;
+	}
+	
+	function testMergingValues() {
+		$p = new Factory('person', array('forename' => 'Carl', 'age' => 30)) ;
+		$this->assertEquals('Carl', $p->get('forename')) ;
+		$this->assertEquals('Wilkins', $p->get('surname')) ;
+		$this->assertEquals(30, $p->get('age')) ;
+		$this->assertEquals('test1@testhost.com', $p->get('email_address')) ;
+	}
+	
+	function testAccessibleAsArray() {
+		$p = new Factory('person') ;
+		$this->assertEquals('Wilkins', $p->data['surname']) ;
+	}
+	
+	function testCanStore() {
+		$this->assertEquals(0, $this->countPeople()) ;
+		$p = new Factory('person') ;
+		$this->assertEquals(1, $p->store()) ;
+		$this->assertEquals(1, $this->countPeople()) ;
+	}
+	
+	function testCanCreate() {
+		$this->assertEquals(0, $this->countPeople()) ;
+		Factory::create('person') ;
+		$this->assertEquals(1, $this->countPeople()) ;
+	}
+	
+	function testCanGetHash() {
+		$a = Factory::hash('person', array('surname' => 'Fowler', 'age' => 26)) ;
+		$this->assertEquals('Jeremy 1', $a['forename']) ;
+		$this->assertEquals('Fowler', $a['surname']) ;
+		$this->assertEquals(26, $a['age']) ;
+	}
+	
+	function testCanCreateMultipleRows() {
+		for($i = 0; $i < 4; $i++)
+			Factory::create('person') ;
+		
+		$this->assertNotEquals(false, $this->findPersonByEmail('test1@testhost.com')) ;
+		$this->assertNotEquals(false, $this->findPersonByEmail('test2@testhost.com')) ;
+		$this->assertNotEquals(false, $this->findPersonByEmail('test3@testhost.com')) ;
+		$this->assertNotEquals(false, $this->findPersonByEmail('test4@testhost.com')) ;
+	}
+	
+	private function countPeople() {
+		($r = mysql_query("SELECT COUNT(*) AS count FROM person")) || die("UNABLE TO COUNT PEOPLE") ;
+		$c = mysql_fetch_assoc($r) ;
+		return $c['count'] ;
+	}
+	
+	private function findPersonByEmail($email) {
+		$r = mysql_query("SELECT * FROM person WHERE email_address = '{$email}'") ;
+		return mysql_fetch_assoc($r) ;
+	}
+	
+}
+
+?>
---
+++ factory_lib.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * This project is licensed under GPLv3.0
+ */
+
+class Factory {
+	
+	protected static $counter = array() ;
+	public $data = null ;
+	public static $factory_data = array() ;
+	private $table_name = null ;
+	
+	public function __construct($table_name, $overrides = array()) {
+		$this->table_name = $table_name ;
+		
+		if (!isset(self::$counter[$this->table_name]))
+			self::$counter[$this->table_name] = 1 ;
+		else
+			self::$counter[$this->table_name]++ ;
+		
+		$this->data = array_merge(self::$factory_data[$this->table_name], $overrides) ;
+		
+		foreach($this->data as $key => $value)
+			if (is_string($value))
+				$this->data[$key] = str_replace('{{counter}}', self::$counter[$this->table_name], $value) ;
+		
+		return $this ;
+	}
+	
+	public function store() {
+		$rowdata = $this->array_to_sql($this->data) ;
+
+		$sql = "INSERT INTO `{$this->table_name}` SET {$rowdata}" ;
+
+		if (mysql_query($sql)) return mysql_insert_id() ;
+
+		if ($GLOBALS['show_db_errors'])
+			die("UNABLE TO INSERT DATA IN insertRow<br />".mysql_error()."<br />$sql") ;
+		else
+			return false ;
+	}
+	
+	public function set($varname, $value) {
+		$this->date[$varname] = $value ;
+		return $this ;
+	}
+	
+	public function get($varname) {
+		return $this->data[$varname] ;
+	}
+	
+	private function escape_column($coldata) {
+		if (is_null($coldata))
+			return 'NULL' ;
+		elseif ($coldata === true)
+			return 'true' ;
+		elseif ($coldata === false)
+			return 'false' ;
+		elseif ($coldata == 'NOW()' || $coldata == 'UNIX_TIMESTAMP()')
+			return $coldata ;
+		else
+			return "'" . mysql_real_escape_string($coldata) . "'" ;
+	}
+	
+	private function join_col_data($col, $data) {
+		return '`' . $col . '` = ' . $this->escape_column($data) ;
+	}
+
+	private function array_to_sql($rowdata, $where_clause = false) {
+		if ($this->is_assoc_array($rowdata)) {
+			$mapped = array() ;
+			foreach($rowdata as $key => $value)
+				$mapped[] = $this->join_col_data($key, $value) ;
+			return implode(', ', $mapped) ;
+		} else
+			return implode(', ', $rowdata) ;
+	}
+	
+	private function is_assoc_array($arr) {
+		return !(array_keys($arr) === range(0, count($arr) - 1, 1)) ;
+	}
+	
+	public static function reset_counter($counter_name = null) {
+		if (is_null($counter_name))
+			self::$counter = array() ;
+		else
+			unset(self::$counter[$counter_name]) ;
+	}
+	
+	public static function create($tablename, $overrides = array()) {
+		$p = new Factory($tablename, $overrides) ;
+		if ($p->store())
+			return $p ;
+		else
+			return false ;
+	}
+	
+	public static function hash($tablename, $overrides = array()) {
+		$p = new Factory($tablename, $overrides) ;
+		return $p->data ;
+	}
+	
+}
+
+?>
